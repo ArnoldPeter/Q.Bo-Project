30ff77abdd5eaed912a36fa75a0b4ae88daa1a07
bennimoser <62222316+bennimoser@users.noreply.github.com>
2020-06-16 11:08:59 +0200

    Update takePicture.py

--- takePicture.py
+++ takePicture.py
@@ -2,6 +2,18 @@ import cv2
 
 cam = cv2.VideoCapture()
 if(cam.IsOpened()):
+    makePicture(cam)
+else:
+    #Say somethings wrong with the camera or try to open the camera
+
+
+
+def makePicture(cam):
+    cam.set(3,1080)
+    cam.set(4,720)
     frame = cam.read()
     cam.Release()
     cv2.imwrite('test.jpg', frame)
+    
+
+    
