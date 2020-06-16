84bda6061c0044281b6b1aa4b3f4f4df0ae23130
bennimoser <62222316+bennimoser@users.noreply.github.com>
2020-06-16 15:20:15 +0200

    Update takePicture.py

--- takePicture.py
+++ takePicture.py
@@ -1,31 +1,45 @@
 import cv2
 from datetime import datetime
 import os
+import schedule
+import time
 
-cam = cv2.VideoCapture(0)
-# 3 =  Enum for Picture Width
-cam.set(3,1080)
-# 4 = Enum for Picture Height
-cam.set(4,720)
-if cam.isOpened():
-    _,frame = cam.read()
-    now = datetime.now()
-    cam.release()
-    imagepath = 'C:\Temp\Testing'
-    if(os.path.exists(imagepath)):
-        print("Directory Exists!")
-        file_name = now.strftime("Images\%m_%d_%Y.%H_%M_%S") + ".jpg"
-        file_path=os.path.join(imagepath,file_name)
+schedule.every().day.at("15:20").do(MakePicture)
+
+
+def MakePicture():
+    cam = cv2.VideoCapture(0)
+    # 3 =  Enum for Picture Width
+    cam.set(3,1080)
+    # 4 = Enum for Picture Height
+    cam.set(4,720)
+    if cam.isOpened():
+        _,frame = cam.read()
+        now = datetime.now()
         cam.release()
-        cv2.imwrite(file_path, frame)
-        cv2.destroyAllWindows()
-    else:
-        print("Directory doesn't exist!")
-        os.mkdir(imagepath)
-        print("Directory made! ")
-        file_name = now.strftime("Images\%m_%d_%Y.%H_%M_%S") + ".jpg"
-        cam.release()
-        cv2.imwrite(os.path.join(imagepath, file_name), frame)
-        cv2.destroyAllWindows()
+        imagepath = 'C:\Temp\Testing'
+        if(os.path.exists(imagepath)):
+            print("Directory Exists!")
+            file_name = now.strftime('%m_%d_%Y.%H_%M_%S') + '.jpg'
+            os.chdir(imagepath)
+            issaved = cv2.imwrite(file_name, frame)
+            if issaved:
+                print("Image " + file_name + " stored in " + imagepath + "!")
+            else:
+                print('An error has occured! Image hasnt been stored ! ')
+            cv2.destroyAllWindows()
+        else:
+            print("Directory " + imagepath +" doesn't exist!")
+            os.mkdir(imagepath)
+            print("Directory " + imagepath +" has been made! ")
+            file_name = now.strftime("%m_%d_%Y.%H_%M_%S") + ".jpg"
+            os.chdir(imagepath)
+            issaved = cv2.imwrite(file_name, frame)
+            if issaved:
+                print("Image " + file_name + " stored in " + imagepath + "!")
+            else:
+                print('An error has occured! Image hasnt been stored ! ')
+            cv2.destroyAllWindows()
+
 
         
