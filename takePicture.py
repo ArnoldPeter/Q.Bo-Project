7efcd12e980d5d6c0aa6df4877b1f192772389b7
bennimoser <62222316+bennimoser@users.noreply.github.com>
2020-06-16 11:19:10 +0200

    Update takePicture.py

--- takePicture.py
+++ takePicture.py
@@ -7,9 +7,10 @@ else:
     #Say somethings wrong with the camera or try to open the camera
 
 
-
 def makePicture(cam):
+    # 3 =  Enum for Picture Width
     cam.set(3,1080)
+    # 4 = Enum for Picture Height
     cam.set(4,720)
     frame = cam.read()
     cam.Release()
