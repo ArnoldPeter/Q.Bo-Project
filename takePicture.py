0cafeb7a393bdd9e28662b6bf0d20ddaf3a04630
bennimoser <62222316+bennimoser@users.noreply.github.com>
2020-06-16 10:46:51 +0200

    Update takePicture.py

--- takePicture.py
+++ takePicture.py
@@ -1,8 +1,7 @@
 import cv2
 
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture()
 if(cam.IsOpened()):
     frame = cam.read()
-    cap.Release()
+    cam.Release()
     cv2.imwrite('test.jpg', frame)
-
