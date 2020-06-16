2e99c16dc384d6fe40a5fc4878349ba703ef066f
bennimoser <62222316+bennimoser@users.noreply.github.com>
2020-06-16 14:48:36 +0200

    Update takePicture.py

--- takePicture.py
+++ takePicture.py
@@ -1,5 +1,6 @@
 import cv2
 from datetime import datetime
+import os
 
 cam = cv2.VideoCapture(0)
 # 3 =  Enum for Picture Width
@@ -9,6 +10,22 @@ cam.set(4,720)
 if cam.isOpened():
     _,frame = cam.read()
     now = datetime.now()
-    file_name = now.strftime("%m_%d_%Y.%H_%M_%S") + ".jpg"
     cam.release()
-    cv2.imwrite(file_name, frame)    
+    imagepath = 'C:\Temp\Testing'
+    if(os.path.exists(imagepath)):
+        print("Directory Exists!")
+        file_name = now.strftime("Images\%m_%d_%Y.%H_%M_%S") + ".jpg"
+        file_path=os.path.join(imagepath,file_name)
+        cam.release()
+        cv2.imwrite(file_path, frame)
+        cv2.destroyAllWindows()
+    else:
+        print("Directory doesn't exist!")
+        os.mkdir(imagepath)
+        print("Directory made! ")
+        file_name = now.strftime("Images\%m_%d_%Y.%H_%M_%S") + ".jpg"
+        cam.release()
+        cv2.imwrite(os.path.join(imagepath, file_name), frame)
+        cv2.destroyAllWindows()
+
+        
