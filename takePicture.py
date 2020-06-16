11febf4c940c148131f8e46bf620d76555da8660
bennimoser <62222316+bennimoser@users.noreply.github.com>
2020-06-16 12:03:31 +0200

    Update takePicture.py

--- takePicture.py
+++ takePicture.py
@@ -1,20 +1,15 @@
 import cv2
+from datetime import datetime
 
-cam = cv2.VideoCapture()
-if(cam.IsOpened()):
-    makePicture(cam)
-else:
-    #Say somethings wrong with the camera or try to open the camera
-
-
-def makePicture(cam):
-    # 3 =  Enum for Picture Width
-    cam.set(3,1080)
-    # 4 = Enum for Picture Height
-    cam.set(4,720)
-    frame = cam.read()
-    cam.Release()
-    cv2.imwrite('test.jpg', frame)
-    
-
-    
+#now = datetime.now()
+#date_time = now.strftime("%m/%d/%Y_%H:%M:%S") + ".jpg"
+#print(date_time)
+cam = cv2.VideoCapture(0)
+# 3 =  Enum for Picture Width
+cam.set(3,1080)
+# 4 = Enum for Picture Height
+cam.set(4,720)
+if cam.isOpened():
+    _,frame = cam.read()
+    cam.release()
+    cv2.imwrite("Testing.jpg", frame)    
