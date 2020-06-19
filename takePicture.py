e0ccc75ccb3320e5048e57b1edfa0accf1ae7461
bennimoser <62222316+bennimoser@users.noreply.github.com>
2020-06-19 11:45:21 +0200

    Update takePicture.py

--- takePicture.py
+++ takePicture.py
@@ -3,12 +3,28 @@ from datetime import datetime
 import os
 import schedule
 import time
+<<<<<<< Updated upstream
 
 def MakePicture():
     #Load config file
     config = yaml.safe_load(open("opt/qbo/config.yml"))
     #set camera and get camera index from config file
     cam = cv2.VideoCapture(int(config['camera']))
+=======
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+def MakePicture():
+    # #Load config file
+    # config = yaml.safe_load(open("opt/qbo/config.yml"))
+    # #set camera and get camera index from config file
+    # cam = cv2.VideoCapture(int(config['camera']))
+    cam = cv2.VideoCapture(0)
+>>>>>>> Stashed changes
     #Set Camera Width on 320 pixels
     cam.set(3, 320) 
     #Set Camera Height on 240 pixels
@@ -33,7 +49,11 @@ def MakePicture():
         cv2.imwrite(fullPath, frame)
         print("Image has been created! ")
         #!
+<<<<<<< Updated upstream
         #Email the picture to qbo.prototyp@gmail.com 
+=======
+        SendMail(fullPath)
+>>>>>>> Stashed changes
         #!
         #Sleep as long as the email needs access to the picture
         time.sleep(3)
@@ -54,7 +74,11 @@ def MakePicture():
         cv2.imwrite(fullPath, frame)
         print("Image has been created! ")
         #!
+<<<<<<< Updated upstream
         #Email the picture to qbo.prototyp@gmail.com 
+=======
+        SendMail(fullPath)
+>>>>>>> Stashed changes
         #!
         #Sleep as long as the email needs access to the picture
         time.sleep(3)
@@ -62,4 +86,34 @@ def MakePicture():
         os.remove(fullPath)
         print("Picture deleted! ")
 
+<<<<<<< Updated upstream
+=======
+def SendMail(fullpath):
+    gmail_user = 'qbo.prototyp@gmail.com'
+    gmail_pw = 'FHWN2700'
+    img_data = open(fullpath, 'rb').read()
+    print("Opened Picture")
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Testing'
+    msg['From'] = 'qbo.prototyp@gmail.com'
+    msg['To'] = 'qbo.prototyp@gmail.com'
+    print("Header Created")
+
+    text = MIMEText("test")
+    msg.attach(text)
+    print("Image created!")
+    image = MIMEImage(img_data, name=os.path.basename(fullpath))
+    msg.attach(image)
+
+    print("Logging into Google! ")
+    s = smtplib.SMTP('smtp.gmail.com',587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(gmail_user,gmail_pw)
+    s.send(From, To, msg.as_string())
+    s.quit
+    print("Sent!")
+
+>>>>>>> Stashed changes
 MakePicture()
