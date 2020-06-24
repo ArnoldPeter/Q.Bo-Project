c1f4de439f0d42a14aefed38c59556926917e039
bennimoser <62222316+bennimoser@users.noreply.github.com>
2020-06-24 13:40:54 +0200

    Update testsms.py

--- testsms.py
+++ testsms.py
@@ -4,13 +4,13 @@ from twilio.rest import Client
 
 def SendSMS(message):
     # the following line needs your Twilio Account SID and Auth Token
-    client = Client("AC3aede748210f547a02e9d406efcadcbb", "31ae8c13f7a19e960e14369a8177417f")
+    client = Client("AC3aede748210f547a02e9d406efcadcbb", "aed1fcdef064d13b76758b162796c342")
 
     # change the "from_" number to your Twilio number and the "to" number
     # to the phone number you signed up for Twilio with, or upgrade your
-    # account to send SMS to any phone number
+    # account to  send SMS to any phone number
     client.messages.create(to="+436642375066", 
                         from_="+12029913651", 
                         body=message)
 
-SendSMS("This is just an example message!")
+SendSMS("Help me! I have fallen! Sent by the Q.Bo Robot! ")
